e6a4a4df22e65d680f34829f8627c1208011c0fe
ManuelForneri <manuelforneri@hotmail.com>
2023-03-06 18:46:49 -0300

    Add ProductCard

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -5,7 +5,7 @@ import { IconButton } from "@material-tailwind/react";
 function CartWidget() {
   return (
     <>
-      <IconButton className="flex bg-transparent text-black text-3xl border-opacity-0 hover:bg-gray-400 shadow-none hover:shadow-none ">
+      <IconButton className="flex bg-transparent text-white hover:text-black text-3xl border-opacity-0 hover:bg-gray-200 shadow-none hover:shadow-none rounded-none ">
         <AiOutlineShoppingCart />
         <span>3</span>
       </IconButton>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-tailwind/react";
 import React from "react";
 import CartWidget from "./CartWidget";
 
@@ -6,10 +7,18 @@ function NavBar() {
     <nav>
       <div className="flex justify-between items-center h-16 p-4 ">
         <div className="flex gap-6 ">
-          <div>Home</div>
-          <div>Products</div>
-          <div>Services</div>
-          <div>Contact</div>
+          <button className="hover:bg-gray-200 p-2 text-white hover:text-black ">
+            Home
+          </button>
+          <button className="hover:bg-gray-200 p-2 text-white hover:text-black ">
+            Products
+          </button>
+          <button className="hover:bg-gray-200 p-2 text-white hover:text-black ">
+            Services
+          </button>
+          <button className="hover:bg-gray-200 p-2 text-white hover:text-black ">
+            Contact
+          </button>
         </div>
         <CartWidget />
       </div>
