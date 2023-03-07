ec4b37543b6c304d1a043af9cc4f57b417b8ab5f
ManuelForneri <manuelforneri@hotmail.com>
2023-03-07 14:05:08 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
 
 function ItemListContainer() {
-  return <div>ItemListContainer</div>;
+  const getProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = response.json();
+
+    return data;
+  };
+  const [products, setProducts] = useState([]);
+  console.log("Products" + products);
+
+  useEffect(() => {
+    getProducts().then((products) => setProducts(products));
+  }, []);
+  return (
+    <div className="bg-blue-gray-800">
+      <ProductCard />
+    </div>
+  );
 }
 
 export default ItemListContainer;
