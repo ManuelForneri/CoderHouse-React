021aa71dcb265592e187fcafda8fe0a3043f0d17
ManuelForneri <manuelforneri@hotmail.com>
2023-03-13 11:39:00 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -17,8 +17,6 @@ function ItemListContainer() {
   }, []);
 
   const catFilter = products.filter((prod) => prod.category === category);
-  /* Buenas, tuve un problema con el filtrado de los productos, 
-  trate de hacerlo como el profe, pero no pude, me sale que data no estaba definida */
 
   return (
     <div className="bg-blue-gray-800 flex flex-wrap gap-8 justify-center p-5 w-full">
