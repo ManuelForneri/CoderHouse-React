da8665da3ddb5e21d82504a86ddb5e1a39c3ee9d
ManuelForneri <manuelforneri@hotmail.com>
2023-03-14 13:28:29 -0300

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="h-screen flex justify-center items-center bg-teal-200">
+      <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white items-center">
+        Bienvenidos a Lola's shop
+      </h1>
+    </div>
+  );
 };
 
 export default Welcome;
