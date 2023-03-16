8a2501eaefb014a62a0790ef28d9612ea9cb5815
ManuelForneri <manuelforneri@hotmail.com>
2023-03-16 12:14:59 -0300

    Revert "add itemDetail"
    This reverts commit 5116cd983f6837a5523a3e975aa1a1e1c3604a4c.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -8,7 +8,6 @@ const ItemList = ({ products }) => {
         return (
           <Item
             key={prod.id}
-            id={prod.id}
             title={prod.title}
             price={prod.price}
             image={prod.image}
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 import { Link, useParams } from "react-router-dom";
+import { Checkbox } from "@material-tailwind/react";
 
 function ItemListContainer() {
   const [products, setProducts] = useState([]);
@@ -24,7 +25,7 @@ function ItemListContainer() {
 
   return (
     <div className="bg-blue-gray-800 flex flex-wrap gap-8 justify-center p-5 w-full">
-      <div className="flex flex-wrap justify-center gap-6 text-white  ">
+      <div className="flex gap-6 text-white  ">
         <Link
           to="/category/men's clothing"
           className="bg-teal-300 p-3 hover:bg-teal-600"
