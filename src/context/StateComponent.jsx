2c07b2b2efc187efd1228c32b0268182e4b0537f
ManuelForneri <manuelforneri@hotmail.com>
2023-03-23 12:12:09 -0300

    Update StateComponent.jsx

--- src/context/StateComponent.jsx
+++ src/context/StateComponent.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { createContext } from "react";
 
-const StateComponent = () => {
-  return <div>StateComponent</div>;
+export const CounterContext = createContext();
+
+const StateComponent = ({ children }) => {
+  const [cart, setCart] = useState(0);
+
+  const increment = () => {
+    setCart(cart + 1);
+  };
+  const decrement = () => {
+    if (cart != 0) {
+      setCart(cart - 1);
+    }
+  };
+  const reset = () => {
+    setCart(0);
+  };
+
+  return (
+    <CounterContext.Provider value={{ cart, increment, decrement, reset }}>
+      {children}
+    </CounterContext.Provider>
+  );
 };
 
 export default StateComponent;
