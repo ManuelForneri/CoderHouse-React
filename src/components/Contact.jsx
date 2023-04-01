bea28a896d17d2ae00460bb742164e1d6adfe15e
ManuelForneri <manuelforneri@hotmail.com>
2023-03-31 21:36:28 -0300

    Se añadio el formulario

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,109 +2,59 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <div className="grid min-h-screen place-items-center">
-        <div className="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12">
-          <h1 className="text-xl font-semibold">
-            Hello there ?,{" "}
-            <span className="font-normal">
-              please fill in your information to continue
-            </span>
-          </h1>
-          <form className="mt-6">
-            <div className="flex justify-between gap-3">
-              <span className="w-1/2">
-                <label
-                  for="firstname"
-                  className="block text-xs font-semibold text-gray-600 uppercase"
-                >
-                  Firstname
-                </label>
-                <input
-                  id="firstname"
-                  type="text"
-                  name="firstname"
-                  placeholder="John"
-                  autocomplete="given-name"
-                  className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-                  required
-                />
-              </span>
-              <span className="w-1/2">
-                <label
-                  for="lastname"
-                  className="block text-xs font-semibold text-gray-600 uppercase"
-                >
-                  Lastname
-                </label>
-                <input
-                  id="lastname"
-                  type="text"
-                  name="lastname"
-                  placeholder="Doe"
-                  autocomplete="family-name"
-                  className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-                  required
-                />
-              </span>
-            </div>
-            <label
-              for="email"
-              className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
-            >
-              E-mail
-            </label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              placeholder="john.doe@company.com"
-              autocomplete="email"
-              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-              required
-            />
-            <label
-              for="password"
-              className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              name="password"
-              placeholder="********"
-              autocomplete="new-password"
-              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-              required
-            />
-            <label
-              for="password-confirm"
-              className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
-            >
-              Confirm password
-            </label>
-            <input
-              id="password-confirm"
-              type="password"
-              name="password-confirm"
-              placeholder="********"
-              autocomplete="new-password"
-              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-              required
-            />
-            <button
-              type="submit"
-              className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none"
-            >
-              Sign up
-            </button>
-            <p className="flex justify-between mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">
-              Already registered?
-            </p>
-          </form>
-        </div>
-      </div>
+    <div className="m-5">
+      <form action="" method="get" className="formulario p-5 ">
+        <label for="nombre" class="form-label">
+          Nombre
+        </label>
+        <input
+          type="text"
+          id="nombre"
+          class="form-input text-dark"
+          required=""
+        />
+
+        <label for="apellido" className="form-label">
+          Apellido
+        </label>
+        <input
+          type="text"
+          id="apellido"
+          class="form-input text-dark"
+          required=""
+        />
+
+        <label for="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          class="form-input text-dark"
+          required=""
+        />
+
+        <label for="asunto" className="form-label">
+          Asunto
+        </label>
+        <input
+          type="text"
+          id="asunto"
+          class="form-input text-dark"
+          required=""
+        />
+
+        <label for="mensaje" className="form-label">
+          Mensaje
+        </label>
+        <textarea
+          id="mensaje"
+          class="form-textarea text-dark"
+          required=""
+        ></textarea>
+
+        <input type="submit" className="btn-submit" value="Enviar" />
+      </form>
     </div>
   );
 };
