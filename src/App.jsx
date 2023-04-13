b4aab891ecf6d7d2c1b7cda2060666c43b56a3a9
ManuelForneri <manuelforneri@hotmail.com>
2023-04-13 12:31:36 -0300

    Add loading word

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,41 @@ import ItemDetailContainer from "./components/ItemDetailContainer";
 import Cart from "./components/Cart";
 import ShoppingCartProvider from "./context/ShoppingCartProvider";
 import Contact from "./components/Contact";
+import Loading from "./components/Loading";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(delay);
+  }, []);
+
   return (
     <ShoppingCartProvider>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Welcome />} />
-          <Route exact path="/products" element={<ItemListContainer />} />
-          <Route
-            exact
-            path="/categoria/:Categoria"
-            element={<ItemListContainer />}
-          />
-          <Route exact path="/item/:id" element={<ItemDetailContainer />} />
-          <Route exact path="/carrito" element={<Cart />} />
-          <Route exact path="/contacto" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Welcome />} />
+            <Route exact path="/products" element={<ItemListContainer />} />
+            <Route
+              exact
+              path="/categoria/:Categoria"
+              element={<ItemListContainer />}
+            />
+            <Route exact path="/item/:id" element={<ItemDetailContainer />} />
+            <Route exact path="/carrito" element={<Cart />} />
+            <Route exact path="/contacto" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
     </ShoppingCartProvider>
   );
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <svg className="loading" viewBox="25 25 50 50">
-        <circle r="20" cy="50" cx="50"></circle>
-      </svg>
+    <div class="spinner">
+      <span>L</span>
+      <span>O</span>
+      <span>A</span>
+      <span>D</span>
+      <span>I</span>
+      <span>N</span>
+      <span>G</span>
     </div>
   );
 };
